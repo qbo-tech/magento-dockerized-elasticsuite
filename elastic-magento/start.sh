b18eafd9c73dcbb25a8909b854e2a44f810fc5dc
Jose <jose@qbo.tech>
2018-01-10 18:27:37 +0000

    Elastic Config Changes

--- elastic-magento/start.sh
+++ elastic-magento/start.sh
@@ -5,5 +5,5 @@ HOSTS=localhost:9200
 CLUSTER_NAME=es
 
 # Deploy configuration
-sed -e "s/CLUSTER_NAME/\"$CLUSTER_NAME\"/;s/HOSTS/$HOSTS/;" /tmp/elasticsearch.yml > /etc/elasticsearch/elasticsearch.yml
+sed -e "s/CLUSTER_NAME/\"$CLUSTER_NAME\"/;s/HOSTS/$HOSTS/;" /tmp/elasticsearch.yml > /usr/share/elasticsearch/config/elasticsearch.yml
 
